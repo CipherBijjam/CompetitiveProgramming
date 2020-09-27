eed2440e2e889695a55b8e2f77b9bb4a3db01dc8
cipherBijjam <cipher.bsd@gmail.com>
2020-09-27 10:18:18 +0530

    Added my basic template which I use in competitive programming in cpp

---
+++ basic_template.cpp
@@ -0,0 +1,30 @@
+// #include <bits/stdc++.h>
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <map>
+
+using namespace std;
+
+#define lli long long
+#define sz(A) (int)A.size()
+#define lsz(A) (long long)A.size()
+#define INT_INF (int)(1e9 + 7)
+
+
+// Compiling command
+// g++ -02 -Wall <question_name>.cpp -o <question_name>
+
+int main(int argc, char const *argv[]){
+
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+
+    
+    
+    return 0;
+}
+
